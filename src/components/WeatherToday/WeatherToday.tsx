eb646779763dba67c32c88fbd20dbc0c5354a3c8
Volha Soika <volhasoika@gmail.com>
2020-03-23 13:45:55 +0300

    feat: add bootstrap, display weather-today

--- src/components/WeatherToday/WeatherToday.tsx
+++ src/components/WeatherToday/WeatherToday.tsx
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 import Skycons from 'react-skycons';
+import { Container, Row, ListGroup } from 'react-bootstrap';
+import CSS from 'csstype';
+
+import './WeatherToday.scss';
 
 interface IWeatherTodayProps {
   data: any;
 }
+
+const skyconsStyles: CSS.Properties = {
+  width: '300px',
+  height: '150px',
+};
 
 class WeatherToday extends Component<IWeatherTodayProps> {
   getIcon = (iconName: string) => {
@@ -13,13 +22,45 @@ class WeatherToday extends Component<IWeatherTodayProps> {
   };
 
   render() {
-    const { summary, icon, temperature, humidity, windSpeed, apparentTemperature } = this.props.data;
+    const {
+      summary,
+      icon,
+      temperature,
+      humidity,
+      windSpeed,
+      apparentTemperature,
+    } = this.props.data;
 
     return (
-      <>
-        {icon ? <Skycons color="green" icon={this.getIcon(icon)} /> : null}
-        {temperature ? <div>{Math.trunc(temperature)}</div> : null}
-      </>
+      <Container>
+        <Row>
+          {temperature ? (
+            <span className="weather-today_temperature">{`${Math.trunc(
+              temperature
+            )}°`}</span>
+          ) : null}
+          {icon ? (
+            <Skycons
+              className="weather_today-icon"
+              color="green"
+              icon={this.getIcon(icon)}
+              style={skyconsStyles}
+            />
+          ) : null}
+          <ListGroup variant="flush">
+            {summary ? (
+              <ListGroup.Item>{summary.toUpperCase()}</ListGroup.Item>
+            ) : null}
+            <ListGroup.Item>
+              FEELS LIKE: {`${Math.trunc(apparentTemperature)}°`}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              WIND: {`${Math.trunc(windSpeed)} m/s`}
+            </ListGroup.Item>
+            <ListGroup.Item>HUMIDITY: {`${humidity * 100} %`}</ListGroup.Item>
+          </ListGroup>
+        </Row>
+      </Container>
     );
   }
 }
